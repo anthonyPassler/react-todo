3e610ac32331d565be7e4b07192c651dcdd6bb3f
anthonyPassler <passleranthony@yahoo.com>
2021-01-26 19:01:18 +0100

    Add state to App and destructured ItemList props

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.scss';
 import Header from './components/Header/Header.js'
 import Item from './components/Item/Item.js'
 import ItemList from './components/ItemList/ItemList.js'
+import React, { useState } from 'react';
 
 const App = () => {
-  const todoMessgaes = ["Wash-up", "Cook Dinner", "work-out"]
+  const [todo, setTodo] = useState(["Wash-up", "Cook Dinner", "work-out"]);
 
   return (
     <div className="banner">
       <Header headline="TODO"/>
       <Item todo={"Cook dinner"}/>
-      <ItemList todoList={todoMessgaes}/>
+      <ItemList todoList={todo}/>
     </div>
   );
 }
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './ItemList.scss';
 import Item from '../Item/Item.js'
 
-const ItemList = (props) => {
+const ItemList = ({ todoList }) => {
   return (
     <div>
-      {props.todoList.map((item, index) => <Item key={index} todo={item}/>)}
+      {todoList.map((item, index) => <Item key={index} todo={item}/>)}
     </div>
   );
 }
